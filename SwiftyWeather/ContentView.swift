64f098ac30a382f39c9351da38b72584d839b4bf
daniel79438 <daniel79438@gmail.com>
2025-03-29 22:02:36 +0000

    Initial Commit

---
+++ SwiftyWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftyWeather
+//
+//  Created by Daniel Harris on 29/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftyWeather/SwiftyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWeatherApp.swift
+//  SwiftyWeather
+//
+//  Created by Daniel Harris on 29/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
