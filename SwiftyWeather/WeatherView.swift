ee7c3f1ffd8a4b2b043b7f9099569cd3b2421f10
daniel79438 <daniel79438@gmail.com>
2025-03-31 22:49:09 +0100

    WeatherView

--- SwiftyWeather/WeatherView.swift
+++ SwiftyWeather/WeatherView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  WeatherView.swift
 //  SwiftyWeather
 //
 //  Created by Daniel Harris on 29/03/2025.
@@ -9,16 +9,47 @@ import SwiftUI
 
 struct WeatherView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack{
+            ZStack {
+                Color(.cyan.opacity(0.75))
+                    .ignoresSafeArea() // full screen
+                VStack (spacing: 0){
+                    
+                    Image(systemName:"cloud.sun.rain.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.horizontal) //padding to the sides only
+                        .symbolRenderingMode(.multicolor) //If the image has 2 colors then this utilises them
+                    
+                    // Text Views
+                    Text("Wild Weather")
+                        .font(.largeTitle)
+                    
+                    Text("42°F")
+                        .font(.system(size: 150, weight: .thin))
+                    
+                    Text("Wind 10mph - Feels Like 36°F")
+                        .font(.title2)
+                        .padding(.bottom) //padding only to the bottom of this text View
+                }
+                .foregroundStyle(.white)
+                .toolbar{
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            //TODO: Add gear click here
+                        } label: {
+                            Image(systemName: "gear")
+                            
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        
     }
 }
-
 #Preview {
     WeatherView()
 }
+
